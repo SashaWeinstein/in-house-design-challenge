e4701ba023a486ddb10b8b59804d00a685e572b6
Alexander Weinstein <aweinstein5@cs.uchicago.edu>
2021-06-03 10:03:25 -0500

    commiting for rollback

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from 'react';
+import './style_old.scss'
 
 function App() {
+
+  // constructor(props){
+  //   this.state = {
+  //     loggedIn: true,
+  //
+  // }
+  // disableButtons(){
+  //
+  // }
+
   return (
     <div>
       <form>
+        <h2>Login</h2>
         <div>
           <label>Username</label>
           <input type='text'></input>
@@ -13,9 +25,27 @@ function App() {
           <input type='password'></input>
         </div>
         <div>
-          <button>Login</button>
+          <button className="login_button">Login</button>
+        </div>
+        <br/>
+      </form>
+      <form>
+        <h2>Create New Account</h2>
+        <div>
+          <label>New Username</label>
+          <input type='text'></input>
+        </div>
+        <div>
+          <label>New Password</label>
+          <input type='password'></input>
+        </div>
+        <div>
+          <button className="create_button">Create Account</button>
         </div>
       </form>
+      <br/>
+      <Button onClick={() => this.props.disable_buttons() }>I Forgot my password</Button>
+
     </div>
   );
 }
