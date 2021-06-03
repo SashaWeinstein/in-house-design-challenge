9f9f251fdea529327a434f381ad74d02cfa32370
Alexander Weinstein <aweinstein5@cs.uchicago.edu>
2021-06-03 10:38:55 -0500

    this is my submission for the NYC CTO in house exercise. I created a scss stylesheet using a combination of basic css, sass variables, and mix-ins. I added a button for 'create account' with the secondary style and a button for 'forgot password' that disables the two buttons. I added an alert that says 'feature not built yet' when the user clicks on either button. I didn't worry about getting the correct fonts or dimensions but otherwise tried to get the buttons to look like the example

--- src/App.js
+++ src/App.js
@@ -1,53 +1,69 @@
 import React from 'react';
-import './style_old.scss'
+import './style.scss'
 
-function App() {
+class App extends React.Component{
 
-  // constructor(props){
-  //   this.state = {
-  //     loggedIn: true,
-  //
-  // }
-  // disableButtons(){
-  //
-  // }
+  constructor() {
+    super();
+    this.state = {
+      user_forgot : false
+    }
 
-  return (
-    <div>
-      <form>
-        <h2>Login</h2>
-        <div>
-          <label>Username</label>
-          <input type='text'></input>
-        </div>
-        <div>
-          <label>Password</label>
-          <input type='password'></input>
-        </div>
-        <div>
-          <button className="login_button">Login</button>
-        </div>
-        <br/>
-      </form>
-      <form>
-        <h2>Create New Account</h2>
-        <div>
-          <label>New Username</label>
-          <input type='text'></input>
-        </div>
-        <div>
-          <label>New Password</label>
-          <input type='password'></input>
-        </div>
-        <div>
-          <button className="create_button">Create Account</button>
-        </div>
-      </form>
-      <br/>
-      <Button onClick={() => this.props.disable_buttons() }>I Forgot my password</Button>
+  }
 
-    </div>
-  );
+  disableButtons(){
+    this.state.user_forgot = !this.state.user_forgot
+    this.forceUpdate()
+  }
+
+  notBuiltYet(){
+    window.alert('sorry, that feature is not built yet')
+  }
+
+  render() {
+    if (!this.state.user_forgot){
+      var reset_password_text = "I forgot my password :("
+    } else{
+      var reset_password_text = "I reset my password ready to login :)"
+    }
+    return (
+        <div>
+          <form>
+            <h2>Login</h2>
+            <div>
+              <label>Username</label>
+              <input type='text'/>
+            </div>
+            <div>
+              <label>Password</label>
+              <input type='password'/>
+            </div>
+            <div>
+              <button onClick={this.notBuiltYet} className="login_button" disabled={this.state.user_forgot}>Login</button>
+            </div>
+            <br/>
+          </form>
+          <form>
+            <h2>Create New Account</h2>
+            <div>
+              <label>New Username</label>
+              <input type='text'/>
+            </div>
+            <div>
+              <label>New Password</label>
+              <input type='password'/>
+            </div>
+            <div>
+              <button onClick={this.notBuiltYet} className="create_button" disabled={this.state.user_forgot}>Create Account</button>
+            </div>
+          </form>
+          <br/>
+          <div>
+            <button className="forgot_button" onClick={() => this.disableButtons()}>{reset_password_text}</button>
+          </div>
+        </div>
+    );
+  }
 }
 
 export default App;
